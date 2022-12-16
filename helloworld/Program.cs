333a964e191598d413e6a5f5af4df39262428890
firat <firatcoskun1733@gmail.com>
2022-12-16 12:47:26 +0300

    Add project files.

---
+++ helloworld/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ders1
+            //Console.WriteLine("Hello World!");
+            /*
+             kodlar yukarıdan aşağıya doğru okunurlar.
+             Büyük küçük harf duyarlılıkları vardır.
+             Console.WriteLine("Merhaba dünya");
+             */
+
+            #endregion
+
+            #region Ders2
+            //Console.Write("Benim adım:");
+            //Console.WriteLine("Firat");
+            //Console.ReadKey();
+            //Console.Write("Soyadım:");
+            //Console.WriteLine("coskun");
+            #endregion
+
+            #region Ders3
+            //Console.ReadLine();
+            //Console.Write("isminiz nedir?");
+            //Console.ReadLine();
+            //Console.Write("soyadiniz nedir?");
+            //Console.ReadLine();
+            //Console.Write("İyi gunler"); 
+            #endregion
+
+            #region Odev1
+            //Console.Write("Kac yasindasiniz?");
+            //Console.ReadLine();
+            //Console.Write("Mesleginiz?");
+            //Console.ReadLine();
+            //Console.Write("Herhangi bir tusa basarak uygulamayı tamamlayabilirsiniz.");
+            //Console.ReadKey(); 
+            #endregion
+
+
+            #region Ders4
+            //string name;
+            //name = "coskun";
+            //Console.WriteLine(name); 
+            #endregion
+
+
+            //string name = Console.ReadLine();
+            //Console.Write(name);
+
+            string name, yname, firstname, fname,  fstname, dudename, gangname, names, broname, truename;
+            Console.WriteLine("10 farkli isim giriniz lütfen:");
+            name = Console.ReadLine();
+
+            yname = Console.ReadLine();
+            firstname = Console.ReadLine();
+            fname = Console.ReadLine();
+            fstname = Console.ReadLine();
+            dudename = Console.ReadLine();
+            gangname = Console.ReadLine();
+            names = Console.ReadLine();
+            broname = Console.ReadLine();
+            truename = Console.ReadLine();
+            Console.WriteLine("girdiginiz isimler: ");
+            Console.WriteLine(name);
+            Console.WriteLine(yname);
+            Console.WriteLine(firstname);
+            Console.WriteLine(fname);
+            Console.WriteLine(fstname);
+            Console.WriteLine(dudename);
+            Console.WriteLine(gangname);
+            Console.WriteLine(names);
+            Console.WriteLine(broname);
+            Console.WriteLine(truename);
+
+
+
+        }
+    }
+}
